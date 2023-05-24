b8147d2bf3962beb57d037a6cb73ee8f0a8f0e60
Mike <mikesystem@hotmail.com>
2023-05-24 15:04:34 -0500

    /

--- main.go
+++ main.go
@@ -21,6 +21,30 @@ type RequestBodyUpdate struct {
 }
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" && r.Method == http.MethodGet {
+			routes := map[string]string{
+				"/-GET":              "https://bc90-186-112-196-136.ngrok-free.app",
+				"/todos-GET":         "https://bc90-186-112-196-136.ngrok-free.app/todos",
+				"/todos-POST":        "https://bc90-186-112-196-136.ngrok-free.app/todos",
+				"/todos/<id>-GET":    "https://bc90-186-112-196-136.ngrok-free.app/todos/<id>",
+				"/todos/<id>-PUT":    "https://bc90-186-112-196-136.ngrok-free.app/todos/<id>",
+				"/todos/<id>-DELETE": "https://bc90-186-112-196-136.ngrok-free.app/todos/<id>",
+				"how does it work":   "https://docs.google.com/document/d/1x4qcJdOrMg3ET55nYUcPBVFEdqY8SgmqAF0KBeD5f4c/edit",
+			}
+			if json, errorJson := json.Marshal(routes); errorJson != nil {
+				http.Error(w, errorJson.Error(), http.StatusInternalServerError)
+				return
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				w.Write(json)
+			}
+		} else {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
+
+	})
 
 	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL.Path
@@ -78,7 +102,6 @@ func main() {
 				http.Error(w, errorAtoi.Error(), http.StatusInternalServerError)
 				return
 			}
-
 			switch method {
 			// - /todos/id - GET
 			case http.MethodGet:
@@ -87,7 +110,6 @@ func main() {
 					http.Error(w, errorID.Error(), http.StatusNotFound)
 					return
 				}
-
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusOK)
 				w.Write(task.Json())
@@ -100,22 +122,17 @@ func main() {
 					http.Error(w, errorBody.Error(), http.StatusInternalServerError)
 					return
 				}
-
 				defer r.Body.Close()
 				var newStatus RequestBodyUpdate
-
 				if errUnmarshal := json.Unmarshal(body, &newStatus); errUnmarshal != nil {
 					http.Error(w, errUnmarshal.Error(), http.StatusInternalServerError)
 					return
 				}
-
 				if errUpdate := f.FindAndUpdateStatus(id, newStatus.Status); errUpdate != nil {
 					http.Error(w, errUpdate.Error(), http.StatusInternalServerError)
 					return
 				}
-
 				w.WriteHeader(http.StatusNoContent)
-
 			// - /todos/id - DELETE
 			case http.MethodDelete:
 				if errorDelete := f.DeleteTask(id); errorDelete != nil {
@@ -123,16 +140,12 @@ func main() {
 					return
 				}
 				w.WriteHeader(http.StatusNoContent)
-
 			default:
 				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-
 			}
-
 		} else {
 			http.Error(w, "404 Not Found, do you mean /todos/id -> id must be an integer?", http.StatusNotFound)
 		}
-
 	})
 
 	if errorListen := http.ListenAndServe(":3000", nil); errorListen != nil {
